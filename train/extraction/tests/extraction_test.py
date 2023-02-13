0482656714cb3d4c8e8d98761c31a5569c183b7c
Siremestir <swan.sauvegrain@gmail.com>
2023-02-13 17:02:37 +0100

    fin deuxieme cours

--- train/extraction/tests/extraction_test.py
+++ train/extraction/tests/extraction_test.py
@@ -1,26 +1,11 @@
-import shutil
 import unittest
-from pathlib import Path
 
-from extraction import extract_images
+class TestStringMethods(unittest.TestCase):
 
-BASE_PATH = Path(__file__).resolve().parent
-output_directory = BASE_PATH / "output"
-input_directory = BASE_PATH / "input"
+    def test_upper(self):
+        value = 'foo'.upper()
+        expected = 'FOO'
+        self.assertEqual(value, expected)
 
-class TestExtraction(unittest.TestCase):
-
-    def tearDown(self):
-        if output_directory.is_dir():
-            shutil.rmtree(str(output_directory))
-
-    def test_pdfs_images_should_be_extracted(self):
-        result = extract_images(str(input_directory), str(output_directory))
-        expected_number_files_input = 2
-        self.assertEqual(result.number_files_input, expected_number_files_input)
-        expected_number_images_output = 3
-        self.assertEqual(result.number_images_output, expected_number_images_output)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
