60c4f703508c6ff7f3f87981f08bb6c4844fda5a
Siremestir <swan.sauvegrain@gmail.com>
2023-02-27 12:13:21 +0100

    rm demo flake8

--- demo_flake8.py
+++
@@ -1,9 +0,0 @@
-print(
-    "hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world"
-)
-from pathlib import Path
-
-raise Exception("Test")
-
-package_dir = Path(__file__).parent.absolute()
-print(package_dir)
